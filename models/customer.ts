65015bc04805bd3a03afdc16e1f320d43081f916
OrysFrey <112826438+OrysFrey@users.noreply.github.com>
2023-05-04 17:59:53 -0500

    Delete models directory

--- models/customer.ts
+++
@@ -1,7 +0,0 @@
-export interface Customer {
-    id: number;
-    nombre_usuario: string;
-    telefono: string;
-    usuario_customer: string;
-    password_customer: string;
-}
--- models/users.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: number;
-    nombre_usuario: string;
-    type_usuario: string;
-    telefono: string;
-    correo_usuario: string;
-    password_usuario: string;
-}
