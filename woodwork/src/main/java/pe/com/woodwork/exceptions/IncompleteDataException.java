9b6e4550dc9ac636415bf9e787edeb17fc268209
ClaudiaPalpan <116833799+ClaudiaPalpan@users.noreply.github.com>
2023-06-25 11:40:25 -0500

    Delete woodwork directory

--- woodwork/src/main/java/pe/com/woodwork/exceptions/IncompleteDataException.java
+++
@@ -1,8 +0,0 @@
-package pe.com.woodwork.exceptions;
-
-public class IncompleteDataException extends RuntimeException{
-
-    public IncompleteDataException() { super(); }
-    public IncompleteDataException(String message) { super(message); }
-
-}
